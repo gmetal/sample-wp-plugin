6d7ba986d54579c4c20f6e258317b65d20fde41d
George Metaxas <gmetaxas@gmail.com>
2017-03-06 22:17:41 +0200

    Initial admin plugin completed

--- sample-wp-plugin.php
+++ sample-wp-plugin.php
@@ -8,40 +8,47 @@
  Author URI: http://localhost/wordpress
  */
 
+add_filter( 'the_title', 'modify_titles');
+
+function modify_titles( $title ) {
+
+	return "<i>".$title."</i>";
+}
+
 add_action( 'admin_notices', 'hello_haec' );
 
 function hello_haec() {
 	$haec_setting = get_option('haec_setting_name');
 	
-	if (!isset($haec_setting)) {
+	if (!isset($haec_setting) || ($haec_setting == '')) {
 		$haec_setting = "HAEC";
 	}
 	
 	echo "<div id='haec'><p>Hello ".$haec_setting." plugin!</p></div>";
 }
 
-
+add_action('admin_menu', 'haec_options_page');
 function haec_options_page() {
 	add_submenu_page(
-			'options-general.php',     // Parent slug (filename)
-			'HAEC Options',            // Page title
-			'HAEC Options menu',       // Menu title
-			'manage_options',          // Capability
-			'haec',                    // menu slug
-			'haec_options_page_html'   // callback function
-			);
+		'options-general.php',     // Parent slug (filename)
+		'HAEC Options',            // Page title
+		'HAEC Options menu',       // Menu title
+		'manage_options',          // Capability
+		'haec',                    // menu slug
+		'haec_options_page_html'   // callback function
+		);
 }
 
-function haec_options_page_html()
-{
+function haec_options_page_html() {
 	// check user capabilities
 	if (!current_user_can('manage_options')) {
 		return;
 	}
 	?>
+
     <div class="wrap">
         <h1><?= esc_html(get_admin_page_title()); ?></h1>
-        <form action="options.php" method="post">
+        <form id="haec_options" action="options.php" method="post">
             <?php
             // output settings fields for the registered setting "haec_options"
             settings_fields('haec_options');
@@ -56,11 +63,13 @@ function haec_options_page_html()
     <?php
 }
 
-add_action('admin_menu', 'haec_options_page');
-
+/**
+ * register haec_settings_init to the admin_init action hook
+ */
+add_action('admin_init', 'haec_settings_init');
 function haec_settings_init() {
 	// register a new setting for "haec" page
-	register_setting('haec',              // option group 
+	register_setting('haec_options',      // option group 
 		  			 'haec_setting_name'  // option name
 			);
 
@@ -72,34 +81,28 @@ function haec_settings_init() {
 			'haec'                      // page
 			);
 
-	// register a new field in the "haec_settings_section" section, inside the "haec" page
+	// register a new field in the "haec_settings_section" 
+	// section, inside the "haec" page
 	add_settings_field(
 			'haec_settings_field',          // id
 			'HAEC Setting',                 // Title
 			'haec_settings_field_cb',       // Callback
-			'haec',                          // page
+			'haec',                         // page
 			'haec_settings_section'         // section
 			);
 }
-
-/**
- * register haec_settings_init to the admin_init action hook
- */
-add_action('admin_init', 'haec_settings_init');
 
 /**
  * callback functions
  */
 
 // section content cb
-function haec_settings_section_cb()
-{
+function haec_settings_section_cb() {
 	echo '<p>HAEC Section Introduction.</p>';
 }
 
 // field content cb
-function haec_settings_field_cb()
-{
+function haec_settings_field_cb() {
 	// get the value of the setting we've registered with register_setting()
 	$setting = get_option('haec_setting_name');
 	// output the field
